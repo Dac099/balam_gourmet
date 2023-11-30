16d1b7e5b17c5c7fda8bf5c92e5d5f1307e09fc7
Dac099 <davidcejazapata@gmail.com>
2023-11-29 19:50:13 -0600

    add busness model

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,12 +1,48 @@
 import { useLoaderData, useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { FaCaretDown } from "react-icons/fa";
 
 export const Products = () => {
   const products = useLoaderData();
   const navigate = useNavigate();
+  const [showRules, setShowRules] = useState(false);
 
   return (
     <section className='pt-16 px-3 mb-10'>
       <p className='text-center text-3xl font-semibold text-violet-700'>Todos nuestros productos</p>
+      
+      <section className='max-w-xl mx-auto my-2 rounded-md bg-violet-100'>
+
+        <div className='flex justify-start gap-1 items-center py-1 px-3'>
+          <FaCaretDown
+            className={`text-xl transition-all duration-300 ${!showRules ? '-rotate-90 ' : 'text-pink-500'}`}
+            onClick={() => setShowRules(!showRules)}
+          />
+          <p className='text-neutral-600 font-bold tracking-wide'>¿Cómo es la venta de nuestros productos?</p>
+        </div>
+
+        <section
+          className={`w-full bg-violet-200 transition-all duration-700  rounded-br-md rounded-bl-md ${showRules ? 'h-[240px] py-2' : 'h-0'}`}
+        >
+          <nav className={`${!showRules ? 'scale-y-0' : 'scale-y-100'} transition-all duration-300`}>
+            <ul className='w-11/12 mx-auto mt-1 pl-1'>
+
+              <li className='text-sm my-2 list-outside list-disc'>
+                Nos especializamos en la venta por mayoreo, ya que nos enfocamos en ventas para locales como pollerias, rosticerias o distribuidores que estén interesados en nuestros productos.
+              </li>
+              
+              <li className='text-sm my-2 list-outside list-disc'>
+                Cuando vendemos nuestros productos, estos pueden ir etiquetados con nuestra marca Balam Gourmet. Sin embargo, si tu negocio lo necesita, el producto puede ir sin etiqueta para que puedas utilizar la etiqueta de tu marca.
+              </li>
+
+            </ul>            
+            <p className='text-xs text-center font-semibold text-violet-800'>Ante cualquier duda no olvides contactarnos</p>         
+          </nav> 
+
+        </section>
+
+      </section>
+
       <Link 
         className='mb-10 p-2 rounded-md block mt-2 underline underline-offset-4 text-pink-500 hover:bg-pink-200 w-max mx-auto transition-all duration-300'
         to={'/contacto'}
